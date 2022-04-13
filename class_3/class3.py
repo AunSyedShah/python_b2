d060ae42bd577cbad4a239a7e65745963823b2b0
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-04-13 23:00:48 +0500

    add round() function

--- class_3/class3.py
+++ class_3/class3.py
@@ -1,6 +1,6 @@
 # arithmetics operators
 n1 = 10
-n2 = 5
+n2 = 3
 add = n1 + n2
 sub = n1 - n2
 mul = n1 * n2
@@ -11,7 +11,7 @@ exp = n1 ** n2
 print(f"{n1} + {n2} = {add}")
 print(f"{n1} - {n2} = {sub}")
 print(f"{n1} * {n2} = {mul}")
-print(f"{n1} / {n2} = {div}")
+print(f"{n1} / {n2} = {round(div, 2)}")
 print(f"{n1} % {n2} = {mod}")
 print(f"{n1} // {n2} = {floor_div}")
 print(f"{n1} ** {n2} = {exp}")
