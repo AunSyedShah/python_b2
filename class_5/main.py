b81aea84705ce6bec77bed0d01a2885fe2bd5404
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-04-14 22:53:45 +0500

    nested if condition

--- class_5/main.py
+++ class_5/main.py
@@ -1,7 +1,6 @@
 percentage = 65
 gender = "female"
 
-if percentage >= 85 or gender == "female":
-    print("scholarship granted")
-else:
-    print("scholarship not granted")
+if percentage >= 85:
+    if gender == "female":
+        print("scholarship granted")
