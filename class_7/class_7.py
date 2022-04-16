264b12226fe9da2b42476026f7024e9570e4f9dc
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-04-16 23:04:04 +0500

    list methods

--- class_7/class_7.py
+++ class_7/class_7.py
@@ -1,6 +1,17 @@
-marks = [75, 67, 56, 98, 87]
+marks = [75, 67, 56, 98, 87, 75]
+#       0    1    2    3    4
 print("list methods")
 print("append method")
 marks.append(100)
 # print list without loop
+print(*marks, sep="\n")
+print("insert method")
+marks.insert(2, 99)
+print("list after insert method")
+print(*marks, sep="\n")
+print("list count method")
+print(marks.count(75))
+print("add element at the starting of the list")
+marks.insert(0, 87)
+print("list after insert method")
 print(*marks, sep="\n")
