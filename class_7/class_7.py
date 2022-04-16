345f3441216f156d69cca571f85e6aabc338086f
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-04-16 22:34:04 +0500

    table of 2

--- class_7/class_7.py
+++ class_7/class_7.py
@@ -1,7 +1,12 @@
 # loop
 # for loop
-from multiprocessing.reduction import steal_handle
 
 
-for i in range(5, 16, 2):
-    print(i)
+for i in range(1, 11):
+    print(f"2 x {i} = {2 * i}")
+
+# 2 x 1 = 2
+# 2 x 2 = 4
+# 2 x 3 = 6
+# 2 x 4 = 8
+# 2 x 5 = 10
