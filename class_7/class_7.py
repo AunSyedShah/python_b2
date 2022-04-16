28ea67fa9b91b319ab829c3eeba988bed91d8858
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2022-04-16 22:41:50 +0500

    accessing last element of list

--- class_7/class_7.py
+++ class_7/class_7.py
@@ -3,7 +3,11 @@
 # List is like an array in other programming languages.
 
 marks = [75, 85, 55, 95, 65]
-print(marks)
+print("specific element from list using index")
+print(marks[3])
+print("to get last element")
+print(marks[-1])
 
+print("printing all the elements in the list using for loop")
 for i in marks:
     print(i)
